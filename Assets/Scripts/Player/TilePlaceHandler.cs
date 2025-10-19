62a6fb04da43a551d20ed786bf10793425fd848a
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-10-19 22:55:42 +0900

    feat : 점수 계산 로직 추가 0.5
    아직 Tile 클래스 및 Joker 클래스가 없는 관계로 해당 부분은 임시 작성이나 수도코드로 작성함
    
    ScoreAccumulator 추가 : 조커는 해당 클래스에 접근하여 값을 수정
    
    TilePlaceHandler에 Delegate 추가 : 특정 작업이 완료되었을 때 전체 시스템에 알리기 위한 델리게이트 추가
    
    고민중
    조커들에 대해 OnTilePlaced를 델리게이트로 호출할 것인지? 혹은 그냥 foreach로 호출할 것인지?

--- Assets/Scripts/Player/TilePlaceHandler.cs
+++ Assets/Scripts/Player/TilePlaceHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,63 @@ public enum eTileEventType
 {
     Placed,
     Destroyed,
+    Removed,
     LineCompleted,
+}
+
+public class ScoreAccumulator
+{
+    [SerializeField] private float baseScore = 0f;
+    [SerializeField] private float bonusScore = 1f;
+    [SerializeField] private float mulValue = 0f;
+
+    public float BaseScore
+    {
+        get => baseScore;
+        set
+        {
+            if (baseScore < value)
+            {
+                // 이펙트 출력
+            }
+
+            baseScore = value;
+        }
+    }
+    
+    public float BonusScore
+    {
+        get => bonusScore;
+        set
+        {
+            if (bonusScore < value)
+            {
+                // 이펙트 출력
+            }
+
+            bonusScore = value;
+        }
+    }
+
+    public float MulValue
+    {
+        get => mulValue;
+        set
+        {
+            if (mulValue < value)
+            {
+                // 이펙트 출력
+            }
+
+            mulValue = value;
+        }
+    }
+    
+    public int GetFinalScore()
+    {
+        float FinalScore = (baseScore + bonusScore) * mulValue;
+        return Mathf.FloorToInt(FinalScore);
+    }
 }
 
 /// <summary>
@@ -17,6 +74,12 @@ public enum eTileEventType
 /// </summary>
 public class TilePlaceHandler : MonoBehaviour
 {
+    // Event Delegate
+    public event Action OnTilePlaceCompleted;
+    public event Action OnTileDestroyCompleted;
+    public event Action OnTileRemoveCompleted;
+    public event Action OnLineCompletionCompleted;
+    
     /// <summary>
     /// 타일에 변화가 있을 때 호출하는 함수
     /// </summary>
@@ -32,6 +95,9 @@ public class TilePlaceHandler : MonoBehaviour
             case eTileEventType.Destroyed:
                 HandleTileDestroyed(tiles);
                 break;
+            case eTileEventType.Removed:
+                HandleTileRemoved(tiles);
+                break;
             case eTileEventType.LineCompleted:
                 HandleLineCompleted(tiles);
                 break;
@@ -41,20 +107,44 @@ public class TilePlaceHandler : MonoBehaviour
     /// <summary>
     /// 타일이 배치되었을 때 후처리 담당
     /// </summary>
-    private void HandleTilePlaced(List<int> placedTiles)
+    private void HandleTilePlaced(in List<int> placedTiles) // List<tile>
     {
-        // TODO
-        // 1. 기본 점수 정의
-        // 2. 조커에 따른 점수 추가
-        // 3. 타일 배치 시 발동하는 조커 트리거
-        // 4. 기타 조커 트리거
-        // * 지속적으로 이펙트 효과 트리거
+        int placeScore = 0;
+        bool isMultiple = false;
+
+        var accumulator = new ScoreAccumulator();
+        
+        // 1. 기본 타일 정보 수집
+        foreach (var tile in placedTiles)
+        {
+            placeScore += tile; // tile.score 로 처리해야함
+
+            // if (tile.Option == ETileOptionType.Multiple)
+            // {
+            //     isMultiple = true;
+            // }
+            
+            // 여기서 각 조커들에 대해 PiecePlaced를 검사할 수도 있다
+        }
+
+        // Multiple 적용 (적용 방식 미정)
+        placeScore = isMultiple ? placeScore * 2 : placeScore;
+        
+        // // #Pseudocode
+        // // 각 조커들에 대해 TilePlaced를 호출
+        // foreach (var Joker in PlayerState.Inventory.Jokers)
+        // {
+        //     Joker.OnTilePlaced(placedTiles, accumulator);
+        // }
+        
+        // 타일이 배치되었음을 전체 시스템애 알림
+        OnTilePlaceCompleted?.Invoke();
     }
 
     /// <summary>
-    /// 타일이 배치되었을 때 후처리 담당
+    /// 타일이 파괴되었을 때 후처리 담당
     /// </summary>
-    private void HandleTileDestroyed(List<int> placedTiles)
+    private void HandleTileDestroyed(in List<int> placedTiles)
     {
         // TODO
         // 1. 기본 점수 정의
@@ -64,10 +154,15 @@ public class TilePlaceHandler : MonoBehaviour
         // * 지속적으로 이펙트 효과 트리거
     }
 
+    private void HandleTileRemoved(in List<int> placedTiles)
+    {
+        
+    }
+
     /// <summary>
-    /// 타일이 완성되었을 때 후처리 담당
+    /// 줄이 완성되었을 때 후처리 담당
     /// </summary>
-    private void HandleLineCompleted(List<int> completedTiles)
+    private void HandleLineCompleted(in List<int> completedTiles)
     { 
         // TODO
         // 1. 기본 점수 정의
