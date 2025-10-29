62ddf6eb2798fd5199ecc57c025eade595272a4f
KimSeungWoo <68574956+machamy@users.noreply.github.com>
2025-10-29 23:49:30 +0900

    Merge pull request #32 from TeamTessellation/feat/events
    FEAT : 이벤트 매니저 추가

---
+++ Assets/Scripts/Events/AsyncPriortyEvent/AsyncEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Events.AsyncPriortyEvent
+{
+    public delegate UniTask AsyncEventHandler();
+    public delegate UniTask AsyncEventHandler<T0>(T0 arg0);
+    public delegate UniTask AsyncEventHandler<T0, T1>(T0 arg0, T1 arg1);
+    public delegate UniTask AsyncEventHandler<T0, T1, T2>(T0 arg0, T1 arg1, T2 arg2);
+    
+
+}
---
+++ Assets/Scripts/Events/AsyncPriortyEvent/AsyncPriorityEvent_1.cs
@@ -0,0 +1,144 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using Events.AsyncPriortyEvent;
+using Events.Core;
+
+namespace Events.AsyncPriorityEvent
+{
+    public class AsyncPriorityEvent<T0> : AsyncPriorityEventBase
+    {
+        private SortedList<int,AsyncEventHandler<T0>> _events = new ();
+    
+        private List<(int,AsyncEventHandler<T0>)> _eventsToAdd = new ();
+        private List<(int,AsyncEventHandler<T0>)> _eventsToRemove = new ();
+    
+        public void AddListener(AsyncEventHandler<T0> listener, int priority = 0)
+        {
+            if (_isInvoking)
+            {
+                _eventsToAdd.Add((priority, listener));
+                return;
+            }
+            if (_events.ContainsKey(priority))
+            {
+                _events[priority] += listener;
+            }
+            else
+            {
+                _events.Add(priority, listener);
+            }
+        }
+    
+        public void RemoveListener(AsyncEventHandler<T0> listener, int priority)
+        {
+            if (_isInvoking)
+            {
+                _eventsToRemove.Add((priority, listener));
+                return;
+            }
+            if (priority == MAGIC_NUMBER)
+            {
+                RemoveListener(listener);
+            }
+            if (_events.ContainsKey(priority))
+            {
+                _events[priority] -= listener;
+            }
+        }
+    
+        public void RemoveListener(AsyncEventHandler<T0> listener)
+        {
+            if (_isInvoking)
+            {
+                _eventsToRemove.Add((0, listener));
+                return;
+            }
+            var keys = new List<int>(_events.Keys);
+            foreach (var k in keys)
+            {
+                _events[k] -= listener;
+            }
+        }
+    
+        public void ClearListeners(int priority)
+        {
+            if (_isInvoking)
+            {
+                _keysToClear.Add(priority);
+                return;
+            }
+            if (_events.ContainsKey(priority))
+            {
+                _events.Remove(priority);
+            }
+        }
+    
+        public void ClearListeners()
+        {
+            if (_isInvoking)
+            {
+                _clearAll = true;
+                return;
+            }
+            _events.Clear();
+        }
+
+        public override void Clear()
+        {
+            ClearListeners();
+        }
+
+        public void Invoke(T0 arg)
+        {
+            InvokeAsync(arg).Forget();
+        }
+    
+        public async UniTask InvokeAsync(T0 arg)
+        {
+            _isInvoking = true;
+            foreach (AsyncEventHandler<T0> e in _events.Values)
+            {
+                foreach (var handler in e.GetInvocationList())
+                {
+                    try
+                    {
+                        await ((AsyncEventHandler<T0>)handler)(arg);
+                    }
+                    catch (Exception ex)
+                    {
+                        UnityEngine.Debug.LogError($"Exception during event invocation: {ex}");
+                    }
+                }
+            }
+            _isInvoking = false;
+        
+            foreach (var (priority, listener) in _eventsToAdd)
+            {
+                AddListener(listener, priority);
+            }
+        
+            foreach (var (priority, listener) in _eventsToRemove)
+            {
+                RemoveListener(listener, priority);
+            }
+        
+            _eventsToAdd.Clear();
+            _eventsToRemove.Clear();
+
+            if (_clearAll)
+            {
+                _events.Clear();
+                _clearAll = false;
+            }
+
+            foreach (var key in _keysToClear)
+            {
+                ClearListeners(key);
+            }
+            _keysToClear.Clear();
+        
+        
+        }
+    }
+}
