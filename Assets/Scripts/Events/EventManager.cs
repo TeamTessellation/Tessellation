cc1be0fbf37982ed83459b8c57d81748c455c586
macham <68574956+machamy@users.noreply.github.com>
2025-10-29 23:41:11 +0900

    feat : 우선순위 이벤트 및 이벤트 매니저 추가

---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,36 @@
+﻿
+using Cardevil.Events;
+using Core;
+using Events.Core;
+using Events.PriorityEvent;
+
+namespace Cardevil.Events
+{
+    
+    /// <summary>
+    /// 이벤트 채널을 관리하는 클래스
+    /// </summary>
+    public class EventManager : Singleton<EventManager>, IClearable
+    {
+        public override bool IsDontDestroyOnLoad { get; }
+
+        protected override void AfterAwake()
+        {
+            Initialize();
+        }
+        
+        PriorityEvent<ScoreChangedEventArgs> _scoreChangedEvent = new PriorityEvent<ScoreChangedEventArgs>();
+
+        protected void Initialize()
+        {
+            _scoreChangedEvent = new PriorityEvent<ScoreChangedEventArgs>();
+        }
+
+        public void Clear()
+        {
+            _scoreChangedEvent.Clear();
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/Events/PriorityEvent/PriorityEventBase.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using Events.Core;
+
+namespace Events.PriorityEvent
+{
+    /// <summary>
+    /// 우선순위 이벤트
+    /// priority의 값이 낮을수록 먼저 호출된다.
+    /// </summary>
+    /// <remarks> 0은 가장 기본적인 이벤트가 등록되어야함. ex : 입장이벤트의 경우, 입장 처리 부분</remarks>
+    public abstract class PriorityEventBase : IClearable
+    {
+        protected const int MAGIC_NUMBER = -321322;
+        
+        protected bool _isInvoking = false;
+        protected List<int> _keysToClear = new List<int>();
+        protected bool _clearAll = false;
+        public abstract void Clear();
+    }
+}
