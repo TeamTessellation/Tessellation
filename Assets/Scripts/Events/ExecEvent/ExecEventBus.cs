c6c888f28c8d4c57a8b4dc3908abf809e65b326d
macham <68574956+machamy@users.noreply.github.com>
2025-11-03 23:46:08 +0900

    feat : Static이벤트버스 추가

--- Assets/Scripts/Events/ExecEvent/ExecEventBus.cs
+++ Assets/Scripts/Events/ExecEvent/ExecEventBus.cs
@@ -5,16 +5,21 @@ using Events.Core;
 namespace PriortyExecEvent
 {
     /// <summary>
-    /// 우선순위 실행 이벤트 버스 클래스
+    /// 우선순위 실행 이벤트 버스 클래스<br/>
+    /// 각 핸들러는 Queue에 작업을 등록하여 우선순위를 지정할 수 있습니다.
     /// </summary>
-    /// <typeparam name="TEvent"></typeparam>
+    /// <remarks>
+    /// 실행시마다 우선순위를 정렬하므로, 빈번한 이벤트 호출에는 적합하지 않을 수 있습니다.
+    /// 해당 경우에는 <see cref="ExecStaticEventBus{TEvent}"/>를 고려하세요.
+    /// </remarks>
+    /// <typeparam name="TEvent"><see cref="ExecEventArgs{T}"/>의 파생 클래스</typeparam>
     public static class ExecEventBus<TEvent> where TEvent : ExecEventArgs<TEvent>, new()
     {
         private static List<ExecEventHandler<TEvent>> _handlers = new List<ExecEventHandler<TEvent>>();
         private static readonly ExecQueue<TEvent> _execQueue = new ExecQueue<TEvent>();
         private static bool _isExecuting = false;
         
-        public static bool IsExecuting => _isExecuting;
+        public static bool IsExecuting => _isExecuting || _execQueue.IsExecuting;
         public static IReadOnlyList<ExecEventHandler<TEvent>> Handlers => _handlers;
         
         
@@ -45,6 +50,25 @@ namespace PriortyExecEvent
             _execQueue.SortByPriority();
             await _execQueue.ExecuteAll(eventArgs);
             _isExecuting = false;
+        }
+        
+        /// <summary>
+        /// 핸들러 실행 후 정적 이벤트 버스도 함께 호출합니다.
+        /// </summary>
+        /// <param name="eventArgs"></param>
+        public static async UniTask InvokeWithStatic(TEvent eventArgs)
+        {
+            _isExecuting = true;
+            _execQueue.Clear();
+            _execQueue.SetCapacity(_handlers.Count);
+            foreach (var handler in _handlers)
+            {
+                handler.Invoke(_execQueue, eventArgs);
+            }
+            _execQueue.SortByPriority();
+            await _execQueue.ExecuteAll(eventArgs);
+            _isExecuting = false;
+            await ExecStaticEventBus<TEvent>.Invoke(eventArgs);
         }
     }
 }
---
+++ Assets/Scripts/Events/ExecEvent/ExecStaticEventBus.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Events.Core;
+
+namespace PriortyExecEvent
+{
+    /// <summary>
+    /// 우선순위 실행 이벤트 버스 클래스.
+    /// 우선순위는 정적이며, 명시적으로 우선순위를 미리 설정해야 합니다.<br/>
+    /// </summary>
+    /// <remarks>
+    /// 기존 <see cref="ExecEventBus{TEvent}"/>와 달리, 핸들러 등록 시 우선순위를 함께 지정하며, 좀더 최적화되어 있습니다.
+    /// </remarks>
+    /// <typeparam name="TEvent"><see cref="ExecEventArgs{T}"/>의 파생 클래스</typeparam>
+    public static class ExecStaticEventBus<TEvent> where TEvent : ExecEventArgs<TEvent>, new()
+    {
+        
+        private static readonly ExecQueue<TEvent> _execQueue = new ExecQueue<TEvent>();
+        public static IReadOnlyList<ExecAction<TEvent>> Handlers => _execQueue;
+        
+        public static bool IsExecuting => _execQueue.IsExecuting;
+        
+        
+        public static void Register(int priority, ExecAction<TEvent> handler)
+        {
+            _execQueue.Enqueue(priority, handler);
+        }
+        public static void Unregister(ExecAction<TEvent> handler)
+        {
+            _execQueue.Remove(handler);
+        }
+        public static void ClearHandlers()
+        {
+            _execQueue.Clear();
+        }
+        
+        public static async UniTask Invoke(TEvent eventArgs)
+        {
+            _execQueue.SortByPriority();
+            await _execQueue.ExecuteAll(eventArgs);
+        }
+    }
+}
