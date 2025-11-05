0fd26dc3358fc211deaf12730ba054998e3a5b7c
macham <68574956+machamy@users.noreply.github.com>
2025-11-05 15:38:20 +0900

    feat : 턴 관련해서 이벤트 브로드캐스트 등록

---
+++ Assets/Scripts/Stage/Events.cs
@@ -0,0 +1,55 @@
+﻿using ExecEvents;
+
+namespace Stage
+{
+    public class TurnStartEventArgs : ExecEventArgs<TurnStartEventArgs>
+    {
+        public TurnStartEventArgs()
+        {
+
+        }
+    }
+    
+    public class TurnEndEventArgs : ExecEventArgs<TurnEndEventArgs>
+    {
+        public TurnEndEventArgs()
+        {
+
+        }
+    }
+    
+    public class PlayerActionLoopStartEventArgs : ExecEventArgs<PlayerActionLoopStartEventArgs>
+    {
+        public PlayerActionLoopStartEventArgs()
+        {
+
+        }
+    }
+    
+    public class PlayerActionLoopEndEventArgs : ExecEventArgs<PlayerActionLoopEndEventArgs>
+    {
+        public PlayerActionLoopEndEventArgs()
+        {
+
+        }
+    }
+    
+    public class BeforePlayerActionEventArgs : ExecEventArgs<BeforePlayerActionEventArgs>
+    {
+        public BeforePlayerActionEventArgs()
+        {
+
+        }
+    }
+    
+    public class StageEndEventArgs : ExecEventArgs<StageEndEventArgs>
+    {
+        public bool IsCleared;
+        
+        public StageEndEventArgs()
+        {
+
+        }
+    }
+
+}
