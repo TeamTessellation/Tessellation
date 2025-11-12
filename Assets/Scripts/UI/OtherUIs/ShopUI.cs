d42db7f3f161e593886e2148daffa3d3898c2227
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-11-13 01:11:42 +0900

    feat : 상점 UI 구현중 1

--- Assets/Scripts/UI/OtherUIs/ShopUI.cs
+++ Assets/Scripts/UI/OtherUIs/ShopUI.cs
@@ -1,16 +1,79 @@
+using System.Collections.Generic;
+using System.Threading;
+using DG.Tweening;
+using Interaction;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ShopUI : MonoBehaviour
+namespace UI.OtherUIs
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ShopUI : UIBase
     {
+        [Header("Shop UI Components")] 
+        [SerializeField] private TMP_Text stageNameText;
+        [SerializeField] private TMP_Text goldText;
         
-    }
+        [Header("Shop Settings")]
+        [SerializeField] private int itemCount = 4;
+        
+        [Header("Tween Settings")] 
+        // TODO..
 
-    // Update is called once per frame
-    void Update()
-    {
+        [Space(20)] 
         
+        [Header("Buttons")] 
+        [SerializeField] private Button _rerollButton;
+        [SerializeField] private Button _skipButton;
+
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
+
+        private List<Tween> currentTweenList = new List<Tween>();
+
+        protected override void Awake()
+        {
+            _rerollButton.onClick.AddListener(OnRerollButtonClicked);
+            _skipButton.onClick.AddListener(OnSkipButtonClicked);
+            gameObject.SetActive(false);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            _tokenSource.Cancel();
+            _tokenSource.Dispose();
+        }
+
+        private void OnEnable()
+        {
+            InteractionManager.Instance.ConfirmEvent += OnConfirmed;
+        }
+        private void OnDisable()
+        {
+            InteractionManager.Instance.ConfirmEvent -= OnConfirmed;
+        }
+        
+        private void OnRerollButtonClicked()
+        {
+            
+        }
+
+        private void OnSkipButtonClicked()
+        {
+            
+        }
+
+        /// <summary>
+        /// 아무 키나 입력이 들어왔을 때 실행되는 로직
+        /// </summary>
+        private void OnConfirmed()
+        {
+            foreach (var tween in currentTweenList)
+            {
+                if(tween.IsActive() && tween.IsPlaying())
+                    tween.Complete();
+            }
+            currentTweenList.Clear();
+        }
     }
 }
