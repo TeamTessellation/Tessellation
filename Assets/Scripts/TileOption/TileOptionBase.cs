efe24ac9c768aeaaff070b0de1ac246c0b4bb045
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2025-11-14 00:09:01 +0900

    feat : 이것저것

---
+++ Assets/Scripts/TileOption/TileOptionBase.cs
@@ -0,0 +1,26 @@
+using Cysharp.Threading.Tasks;
+using System.Drawing;
+using UnityEngine;
+
+public abstract class TileOptionBase
+{
+    public virtual async UniTask OnTilePlaced(Tile tile)
+    {
+
+    }
+
+    public virtual async UniTask OnLineCleared(Tile tile)
+    {
+
+    }
+
+    public virtual async UniTask OnTileRemoved(Tile tile)
+    {
+
+    }
+
+    public virtual async UniTask OnTileBurst(Tile tile)
+    {
+
+    }
+}
