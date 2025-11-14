8e66758e0df43bf73fe6ac2c2777cd769e58f847
macham <68574956+machamy@users.noreply.github.com>
2025-11-15 03:28:04 +0900

    chore : 빌드용

---
+++ Assets/Database/Generated/StringData.cs
@@ -0,0 +1,17 @@
+using System.Text;
+using System;
+using System.Collections.Generic;
+
+namespace Database.Generated
+{
+
+    [UnityEngine.Scripting.Preserve]
+    [Serializable]
+    public partial class StringData: IDBData {
+
+        /// <summary> 아이템 아이디 </summary>
+        public string ItemID;
+        /// <summary> 한국어 </summary>
+        public string kr;
+    }
+}
