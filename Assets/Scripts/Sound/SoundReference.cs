be1d3bb969acfeca9b66c43adf24bd95d5759fef
gerwookwon <gerwookwon@gmail.com>
2025-11-17 01:05:59 +0900

    효과음 싹다OGG로 교체, GoldGet 볼륨 감소

--- Assets/Scripts/Sound/SoundReference.cs
+++ Assets/Scripts/Sound/SoundReference.cs
@@ -7,23 +7,23 @@
         public static readonly SoundReference InGameBGM = new SoundReference("BGM/Ingame");
         
         // SFX
-        public static readonly SoundReference GameOver = new SoundReference("SFX/GameOver_V2");
-        public static readonly SoundReference GoldGet = new SoundReference("SFX/GoldGet_V2");
-        public static readonly SoundReference MenuSetting = new SoundReference("SFX/MenuSetting_V2");
-        public static readonly SoundReference NumberIncrease = new SoundReference("SFX/NumberIncrease_V2");
-        public static readonly SoundReference RoundClear = new SoundReference("SFX/RoundClear_V2");
-        public static readonly SoundReference SceneTransitionIn = new SoundReference("SFX/SceneTransitionIn_V2");
-        public static readonly SoundReference SceneTransitionOut = new SoundReference("SFX/SceneTransitionOut_V2");
-        public static readonly SoundReference TileBomb = new SoundReference("SFX/TileBomb_V2");
-        public static readonly SoundReference TileClear = new SoundReference("SFX/TileClear_V2");
-        public static readonly SoundReference TileGold = new SoundReference("SFX/TileGold_V2");
-        public static readonly SoundReference TileMultiple = new SoundReference("SFX/TileMultiple_V2");
-        public static readonly SoundReference TileOptionTrigger = new SoundReference("SFX/TileOptionTrigger_V2");
-        public static readonly SoundReference TilePickup = new SoundReference("SFX/TilePickup_V2");
-        public static readonly SoundReference TileRelease = new SoundReference("SFX/TileRelease_V2");
-        public static readonly SoundReference TileRotateSelect = new SoundReference("SFX/TileRotateSelect_V2");
-        public static readonly SoundReference TileRotate = new SoundReference("SFX/TileRotate_V2");
-        public static readonly SoundReference UIClick = new SoundReference("SFX/UIClick_V2");
+        public static readonly SoundReference GameOver = new SoundReference("SFX/GameOver");
+        public static readonly SoundReference GoldGet = new SoundReference("SFX/GoldGet");
+        public static readonly SoundReference MenuSetting = new SoundReference("SFX/MenuSetting");
+        public static readonly SoundReference NumberIncrease = new SoundReference("SFX/NumberIncrease");
+        public static readonly SoundReference RoundClear = new SoundReference("SFX/RoundClear");
+        public static readonly SoundReference SceneTransitionIn = new SoundReference("SFX/SceneTransitionIn");
+        public static readonly SoundReference SceneTransitionOut = new SoundReference("SFX/SceneTransitionOut");
+        public static readonly SoundReference TileBomb = new SoundReference("SFX/TileBomb");
+        public static readonly SoundReference TileClear = new SoundReference("SFX/TileClear");
+        public static readonly SoundReference TileGold = new SoundReference("SFX/TileGold");
+        public static readonly SoundReference TileMultiple = new SoundReference("SFX/TileMultiple");
+        public static readonly SoundReference TileOptionTrigger = new SoundReference("SFX/TileOptionTrigger");
+        public static readonly SoundReference TilePickup = new SoundReference("SFX/TilePickup");
+        public static readonly SoundReference TileRelease = new SoundReference("SFX/TileRelease");
+        public static readonly SoundReference TileRotateSelect = new SoundReference("SFX/TileRotateSelect");
+        public static readonly SoundReference TileRotate = new SoundReference("SFX/TileRotate");
+        public static readonly SoundReference UIClick = new SoundReference("SFX/UIClick");
         
         private readonly string path;
 
