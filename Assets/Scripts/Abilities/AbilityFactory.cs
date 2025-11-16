e6713cb164491935a357ac980e251beb0883f8db
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-11-16 16:51:03 +0900

    feat : ActiveUpAbility 완료

--- Assets/Scripts/Abilities/AbilityFactory.cs
+++ Assets/Scripts/Abilities/AbilityFactory.cs
@@ -7,27 +7,33 @@ namespace Abilities
 {
     public static class AbilityFactory
     {
-        private static Dictionary<eItemType, Func<AbilityBase>> abiltyFuncs = new()
+        private static Dictionary<eItemType, Func<AbilityBase>> abilityFuncs = new()
         {
-            { eItemType.ExtraTurn, () => new OnlyPlaceAbility() },
-            { eItemType.ExtraActiveItemRemainingUses, () => new OnlyPlaceAbility() },
-            { eItemType.AddBombTileset, () => new OnlyPlaceAbility() },
-            { eItemType.IncreaseExplosionRange, () => new OnlyPlaceAbility() },
-            { eItemType.BombImmediatelyExplosion, () => new OnlyPlaceAbility() },
-            { eItemType.ChainExplosion, () => new OnlyPlaceAbility() },
-            { eItemType.AddExtraScoreTileset, () => new OnlyPlaceAbility() },
-            { eItemType.AddMultipleScoreTileset, () => new OnlyPlaceAbility() },
-            { eItemType.AddGoldTileset, () => new OnlyPlaceAbility() },
-            { eItemType.GoldTilesetCoinScaledExtraScore, () => new OnlyPlaceAbility() },
-            { eItemType.AdditionalInterest, () => new OnlyPlaceAbility() },
-            { eItemType.CoinScaledMultiple, () => new OnlyPlaceAbility() },
+            { eItemType.ExtraTurn,                        () => new OnlyPlaceAbility() },
+            { eItemType.ExtraActiveItemRemainingUses,     () => new OnlyPlaceAbility() },
+            { eItemType.AddBombTileset,                   () => new OnlyPlaceAbility() },
+            { eItemType.IncreaseExplosionRange,           () => new OnlyPlaceAbility() },
+            { eItemType.BombImmediatelyExplosion,         () => new OnlyPlaceAbility() },
+            { eItemType.ChainExplosion,                   () => new OnlyPlaceAbility() },
+            { eItemType.AddExtraScoreTileset,             () => new OnlyPlaceAbility() },
+            { eItemType.AddMultipleScoreTileset,          () => new OnlyPlaceAbility() },
+            { eItemType.AddGoldTileset,                   () => new OnlyPlaceAbility() },
+            { eItemType.GoldTilesetCoinScaledExtraScore,  () => new OnlyPlaceAbility() },
+            { eItemType.AdditionalInterest,               () => new OnlyPlaceAbility() },
+            { eItemType.CoinScaledMultiple,               () => new OnlyPlaceAbility() },
+            { eItemType.GetTilesetDelete,                 () => new ActiveUpAbility() },
+            { eItemType.GetTilesetReroll,                 () => new ActiveUpAbility() },
+            { eItemType.GetRevert,                        () => new ActiveUpAbility() },
+            { eItemType.GetTilesetRotate,                 () => new ActiveUpAbility() },
+            { eItemType.GetTilesetChangeOverwrite,        () => new ActiveUpAbility() },
+            { eItemType.GetTilesetCopy,                   () => new ActiveUpAbility() },
         };
         
         public static AbilityBase Create(AbilityDataSO abilitydata)
         {
-            if (!abiltyFuncs.ContainsKey(abilitydata.ItemType)) return null;
+            if (!abilityFuncs.ContainsKey(abilitydata.ItemType)) return null;
 
-            AbilityBase newAbility = abiltyFuncs[abilitydata.ItemType]();
+            AbilityBase newAbility = abilityFuncs[abilitydata.ItemType]();
             newAbility.InitializeData(abilitydata);
             
             return newAbility;
