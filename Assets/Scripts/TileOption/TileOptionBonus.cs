12c229dcb2257d681fca8fe982fd3065cb9366ae
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-11-17 23:44:05 +0900

    feat : 배치소리 + 아이템초기화

--- Assets/Scripts/TileOption/TileOptionBonus.cs
+++ Assets/Scripts/TileOption/TileOptionBonus.cs
@@ -1,14 +1,28 @@
 ﻿
 
 using Cysharp.Threading.Tasks;
+using Sound;
 
 public class TileOptionBonus : TileOptionBase
 {
+    public override async UniTask OnTilePlaced(Tile tile)
+    {
+        int baseScore = (int)ScoreManager.Instance.ScoreValues[ScoreManager.ScoreValueType.BasePlaceScore];
+        int finalScore = ScoreManager.Instance.CalculateTileScore(eTileEventType.Place, tile, baseScore);
+        ScoreManager.Instance.AddCurrentScore(finalScore);
+        
+        ShowScoreEffect(finalScore, tile);
+        
+        SoundManager.Instance.PlaySfx(SoundReference.TileRelease);
+    }
+
     public override async UniTask OnLineCleared(Tile tile)
     {
         int baseScore = (int)ScoreManager.Instance.ScoreValues[ScoreManager.ScoreValueType.BaseBonusScore];
         int finalScore = ScoreManager.Instance.CalculateTileScore(eTileEventType.LineClear, tile, baseScore);
         ScoreManager.Instance.AddCurrentScore(finalScore);
+        
+        ShowScoreEffect(finalScore, tile);
     }
 
     public override async UniTask OnTileBurst(Tile tile)
@@ -16,5 +30,7 @@ public class TileOptionBonus : TileOptionBase
         int baseScore = (int)ScoreManager.Instance.ScoreValues[ScoreManager.ScoreValueType.BaseLineClearMultiple];
         int finalScore = ScoreManager.Instance.CalculateTileScore(eTileEventType.Burst, tile, baseScore);
         ScoreManager.Instance.AddCurrentScore(finalScore);
+        
+        SoundManager.Instance.PlaySfx(SoundReference.TileBomb);
     }
 }
