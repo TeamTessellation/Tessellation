b9dcb17fe2a8f605ee23942baf427a207abda07d
macham <68574956+machamy@users.noreply.github.com>
2025-11-17 12:14:29 +0900

    build : 빌드 최적화

---
+++ Assets/Editor/TextureOptimizer.cs
@@ -0,0 +1,25 @@
+﻿// 텍스처 압축 자동화
+
+using UnityEditor;
+#if UNITY_WEBGL
+public class TextureOptimizer : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        var importer = assetImporter as TextureImporter;
+        
+        // WebGL 플랫폼 설정
+        var platformSettings = new TextureImporterPlatformSettings
+        {
+            name = "WebGL",
+            overridden = true,
+            maxTextureSize = 1024, // 모바일 고려
+            format = TextureImporterFormat.DXT5,
+            compressionQuality = 80, // 품질 vs 크기 균형
+            allowsAlphaSplitting = true
+        };
+        
+        importer.SetPlatformTextureSettings(platformSettings);
+    }
+}
+#endif
