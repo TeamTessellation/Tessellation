962d161698c3309a980cd36534facdd68f2dbbe6
macham <68574956+machamy@users.noreply.github.com>
2026-08-04 22:53:23 +0900

    fix: cancel pooled tile effect tweens

--- Assets/Scripts/HexaTile/Tile/Tile.cs
+++ Assets/Scripts/HexaTile/Tile/Tile.cs
@@ -121,6 +121,7 @@ public class Tile : MonoBehaviour, IPoolAble<TileData>
 
         float progress = effectData.LightStartPower;
         await DOTween.To(() => progress, x => { SetLight(x); progress = x; }, effectData.LightMaxPower, effectData.LightDuration)
+            .SetTarget(this)
             .SetEase(effectData.LightEase)
             .ToUniTask();
 
@@ -155,6 +156,7 @@ public class Tile : MonoBehaviour, IPoolAble<TileData>
         _sr.color = effectData.FadeOutColor;
         float progress = 1;
         await DOTween.To(() => progress, x => { _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, x); progress = x; }, 0, effectData.FadeOutDuration)
+            .SetTarget(this)
             .SetEase(effectData.FadeOutEase)
             .ToUniTask();
 
@@ -172,6 +174,7 @@ public class Tile : MonoBehaviour, IPoolAble<TileData>
         _light.color = effectData.LightColor;
         float progress = effectData.LightStartPower;
         await DOTween.To(() => progress, x => { SetLight(x); progress = x; }, effectData.LightMaxPower, effectData.LightDuration)
+            .SetTarget(this)
             .SetEase(effectData.LightEase)
             .ToUniTask();
 
